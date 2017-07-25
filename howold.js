cc449b5e34884451c307dfcb2d249c003dadfe23
Robbie Shock <rob.f.shock@gmail.com>
2017-07-24 20:02:46 -0500

    finished the how old exercise

---
+++ howold.js
@@ -0,0 +1,62 @@
+console.log("linked");
+
+// 1 How many hours are in a year
+
+var daysInYear;
+var hoursInDay;
+console.log(daysInYear * hoursInDay);
+
+// 2 How many minutes are in a decade
+
+var yearsInDecade = 10;
+var minutesInHour = 60;
+console.log("minutes in decade", yearsInDecade * daysInYear * hoursInDay * minutesInHour);
+
+// 3 How many seconds old am i 
+
+var secondsInMinute = 60;
+var ageOfYoda = 43;
+var ageInSeconds = ageOfYoda * daysInYear * hoursInDay * minutesInHour * secondsInMinute;
+console.log("How old Yoda is in seconds:", ageInSeconds);
+
+// 4 if they are older than some arbitrary number, console "I'm old", else "I'm young"
+
+if (ageInSeconds > 982749873){
+	console.log("I'm old!");
+} else {
+	console.log("I'm young");
+}
+
+
+
+
+
+
+
+console.log(24*7*52);
+
+console.log(60*24*365*10);  
+// 5256000
+console.log(60*60*24);
+// 86400
+console.log(86400*365);
+// 31,536,000
+console.log(31536000*34);
+// 1072224000
+console.log(1072224000+86400*335);
+// 1,101,168,000
+console.log(1101168000+60*60*19+60*18);
+// 1101237480
+
+var secondsOld = 1101237480;
+if (secondsOld > 1000000000) {
+	console.log("You're quite old.  Like a decade older than her.")
+}
+
+1,101,237,480
+
+console.log (60*60*24*365*10);315,360,000
+
+var daysInYear;
+var hoursInDay;
+console.log(daysInYear * hoursInDay);
